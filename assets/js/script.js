40812760f66baf206e61eafc5aa9ad8510562ebc
Dempoleon <dempoleon@gmail.com>
2022-10-13 15:15:33 -0700

    added function to get next game tickets for desired team called getNextGameTickets(team, league)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 //Gets all games of the season
+//takes in parameter season year: year of the desired season
+//console.log all nba regular season games of that season
 var nbaApiKey = config.nbaKey;
-async function getAllGames(seasonYear) {
+async function nbaGetSeasonGames(seasonYear) {
 
     var apiURL = 'https://api.sportsdata.io/v3/nba/scores/json/Games/' + seasonYear + '?key=' + nbaApiKey;
 
@@ -22,8 +24,8 @@ async function getAllGames(seasonYear) {
 
 }
 
-//Gets all games for the home team
-async function getHomeGames(seasonYear, homeTeam) {
+//Gets all home games for a given team
+async function nbaGetHomeGames(seasonYear, homeTeam) {
 
     var apiURL = 'https://api.sportsdata.io/v3/nba/scores/json/Games/' + seasonYear + '?key=' + nbaApiKey;
 
@@ -200,12 +202,137 @@ async function getLastGameScore(seasonYear, teamName) {
         });
 }
 
-function getTicketMaster() {
+function getTicketMaster(sport) {
 
+    // var apiURL = 'https://app.ticketmaster.com/discovery/v2/events.json?keyword='+sport+'&sort=date,asc&apikey=' + tmKey;
+    var apiURL = 'https://app.ticketmaster.com/discovery/v2/events.json?promoterId='+sport+'&sort=date,asc&apikey=' + tmKey;
+    fetch(apiURL)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+
+                    //todo: get last played game score
+                    var games = data['_embedded'].events;
+                    // console.log(data['_embedded'].events);
+                    // var eventDates = [];
+                    games.forEach(game => {
+                        console.log(game);
+                        console.log(game['url']);
+                    });
+
+                });
+            } else {
+                console.log('error: ' + response.statusText);
+            }
+        })
+        .catch(function (error) {
+            console.log('unable to connect to api link');
+        });
 }
 
-// getLastGameScore(2022, 'GS')
+// function to get ticketmaster url link to the next game of a specific team;
+// TODO: do more filtering to get the following results:
+// game date, teams involved, url to ticketmaster
+function getTicketMaster(sport, teamName) {
 
-// var gameInfo = await getNextHomeGameDate(2023, 'GS');
-// // getNextHomeGame(2023, 'GS');
-// console.log(gameInfo);
+    var apiURL = 'https://app.ticketmaster.com/discovery/v2/events.json?keyword='+sport+'&sort=date,asc&apikey=' + tmKey;
+    // var apiURL = 'https://app.ticketmaster.com/discovery/v2/events.json?promoterId='+sport+'&sort=date,asc&apikey=' + tmKey;
+    fetch(apiURL)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+
+                    //todo: get last played game score
+                    var games = data['_embedded'].events;
+                    // console.log(data['_embedded'].events);
+                    // var eventDates = [];
+                    games.forEach(game => {
+                        // if the game promoter exists
+                        if(game.promoter) {
+                            var promoters = game.promoter.name.split(' ');
+                            // console.log(game.promoter.name.split(' '))
+                            if(promoters.includes(sport.toUpperCase())) {
+                                console.log(game.name.toUpperCase().split()); 
+                                // console.log(game.dates)
+                                // if(game.name.toUpperCase().split().includes(teamName.toUpperCase())) {
+                                //     console.log(game.name)
+                                // }
+                            }
+                        }
+                        
+                        // if(game.promoter) {
+                        //     // console.log(game.promoter.name.split(' '));
+                        //     console.log(game.promoter['name'])
+                        //     // if(game.promoter.name.split(' ').includes(sport.toUpperCase())) {
+                        //     //     if (game.prmoter.name) {
+                        //     //         console.log(game.promoter.name)
+                        //     //     }
+                        //     // }
+                        // }
+                        // console.log(game.promoter)
+                        // if (game.promoter['name'].split(' ').includes(sport.toUpperCase())) {
+                        //     console.log(game.promoter)
+                        //     // console.log(game.promoter['name'])
+                        // }
+                        // console.log(game.promoter);
+                        // console.log(game['url']);
+                    });
+
+                });
+            } else {
+                console.log('error: ' + response.statusText);
+            }
+        })
+        .catch(function (error) {
+            console.log('unable to connect to api link');
+        });
+}
+
+
+//
+// uses ticketmaster api
+// can access: game name, game date, ticket url, venue name;
+async function getNextGameTickets(team, league) {
+
+    var apiURL = 'https://app.ticketmaster.com/discovery/v2/events.json?keyword='+team+'&sort=date,asc&apikey=' + tmKey;
+
+    fetch(apiURL)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    let allEvents = data['_embedded'].events;
+                    console.log(allEvents);
+                    allEvents.forEach(game => {
+                        if(game.promoter) {
+                            let gamePromoters = game.promoter.name.split(' ');
+                            // console.log(gamePromoters)
+                            
+                            if(gamePromoters.includes(league.toUpperCase())) {
+                                let gameDate = game.dates.start.localDate;
+                                let gameName = game.name;
+                                let gameVenue = game._embedded.venues[0].name;
+                                let ticketUrl = game.url
+
+                                console.log(gameDate);
+                                console.log(gameName);
+                                console.log(gameVenue);
+                                console.log(ticketUrl);
+                                
+                            }
+
+                        }
+                    });
+                    // console.log()
+                });
+            } else {
+                console.log('error: ' + response.statusText);
+            }
+        })
+        .catch(function (error) {
+            console.log('unable to connect to api link');
+        });
+}
+
+
+getNextGameTickets('dodgers', 'mlb')
+
