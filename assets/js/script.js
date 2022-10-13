5e53e73061379521d8c790341932ef50b047f295
Angellyn218 <angellyncervantes@yahoo.com>
2022-10-13 01:37:24 -0700

    load games into storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,9 @@ var savedGames = [];
 var gamesEl = $('#games'); // Change depending on name of section with upcoming games
 
 function loadGamesIntoStorage() {
-     
+    var game = $(this).parent(); // Change based on how game element is made
+    savedGames.push(game);
+    localStorage.setItem('saved games', JSON.stringify(savedGames));
 }
 
 // .saveBtn is name of button for saving specific game. change based on name of button
