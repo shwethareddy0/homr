fc1835a1018b4511de397fc98db17e6a832f02d6
Dempoleon <dempoleon@gmail.com>
2022-10-13 12:24:18 -0700

    filtering api data responses

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,7 @@ async function getAllGames(seasonYear) {
             if (response.ok) {
                 response.json().then(function (data) {
                     console.log(data);
+                    
                 });
             } else {
                 console.log('error: ' + response.statusText);
@@ -74,7 +75,7 @@ async function getTickets() {
 
 async function getEventforVenue(venueID) {
 
-    // var apiURL = 'https://app.ticketmaster.com/discovery/v2/venues.json?keyword=chase&apikey=' + tmKey;
+    var apiURL = 'https://app.ticketmaster.com/discovery/v2/venues.json?keyword=chase&apikey=' + tmKey;
     // https://app.ticketmaster.com/discovery/v2/events.json?apikey={apikey}
     // var apiURL = 'https://app.ticketmaster.com/discovery/v2/venues/'+ venueID +'.json?keyword=chase&apikey=' + tmKey;
 
@@ -96,8 +97,115 @@ async function getEventforVenue(venueID) {
 
 
 
-getHomeGames(2023, 'GS');
+// getHomeGames(2023, 'GS');
 // getTickets();
-getEventforVenue('KovZ917Ah1H');
+// getEventforVenue('KovZ917Ah1H');
 
 
+var testHomegames = []
+async function getNextHomeGameDate(seasonYear, homeTeam) {
+
+    var apiURL = 'https://api.sportsdata.io/v3/nba/scores/json/Games/' + seasonYear + '?key=' + nbaApiKey;
+
+    fetch(apiURL)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    var arr = [];
+                    data.forEach(game => {
+                        if (game.HomeTeam === homeTeam) {
+                            if (game.Status === 'Scheduled') {
+                                // console.log(game.Day);
+                                arr.push(game.Day);
+                            }
+                        }
+                    }
+                    
+                    
+                    );
+                    console.log(arr);
+                    return arr;
+                   
+                });
+            } else {
+                console.error('error: ' + response.statusText);
+            }
+        })
+        .catch(function (error) {
+            console.error('unable to connect to api link');
+        });
+}
+
+async function uberApi(seasonYear, homeTeam) {
+
+    https://api.uber.com/v1.2/estimates/price?start_latitude=37.7752315&start_longitude=-122.418075&end_latitude=37.7752415&end_longitude=-122.518075
+    var apiURL = 'https://api.sportsdata.io/v3/nba/scores/json/Games/' + seasonYear + '?key=' + nbaApiKey;
+
+
+    fetch(apiURL)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    var arr = [];
+                    data.forEach(game => {
+                        if (game.HomeTeam === homeTeam) {
+                            if (game.Status === 'Scheduled') {
+                                // console.log(game.Day);
+                                arr.push(game.Day);
+                            }
+                        }
+                    }
+                    
+                    
+                    );
+                    console.log(arr);
+                    return arr;
+                   
+                });
+            } else {
+                console.error('error: ' + response.statusText);
+            }
+        })
+        .catch(function (error) {
+            console.error('unable to connect to api link');
+        });
+}
+
+
+async function getLastGameScore(seasonYear, teamName) {
+
+    var apiURL = 'https://api.sportsdata.io/v3/nba/scores/json/Games/' + seasonYear + '?key=' + nbaApiKey;
+
+
+    fetch(apiURL)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+
+                    //todo: get last played game score
+                    data.forEach(game => {
+                        if(game.HomeTeam === teamName) {
+                            console.log(game);
+                        }
+                        
+                    });
+
+                });
+            } else {
+                console.log('error: ' + response.statusText);
+            }
+        })
+        .catch(function (error) {
+            console.log('unable to connect to api link');
+        });
+}
+
+function getTicketMaster() {
+
+}
+
+// getLastGameScore(2022, 'GS')
+
+// var gameInfo = await getNextHomeGameDate(2023, 'GS');
+// // getNextHomeGame(2023, 'GS');
+// console.log(gameInfo);
